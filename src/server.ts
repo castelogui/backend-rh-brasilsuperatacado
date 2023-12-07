bc541e81c6798d0606ff63fabc58d9f9e83840d4
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-07 17:26:50 -0400

    feat: Add function connect before running server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import "reflect-metadata";
-import { AppDataSource } from "./database/AppDataSource";
 import { app } from "./app";
+import { connect } from "./database/connect";
 require("dotenv").config();
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Server connected to the database");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to database: ", err);
-  });
-
-app.listen(process.env.PORT, () => console.log("Server is running"));
+connect().then(() => {
+  app.listen(process.env.PORT, () => console.log("Server is running"));
+});
