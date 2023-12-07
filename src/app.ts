6f77a6b52c5628af77f0d2c60de12440640131a0
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-07 16:55:38 -0400

    add app

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { NextFunction, Request, Response } from "express";
+import { routes } from "./routes";
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err}`,
+    });
+  }
+);
+
+export { app };
