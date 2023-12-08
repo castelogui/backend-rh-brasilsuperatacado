eb781631b2b75b829c3575bef20e849e4d0127db
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-07 22:49:36 -0400

    fix: add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 import { app } from "./app";
 import { connect } from "./database/connect";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 connect().then(() => {
   app.listen(process.env.PORT, () => console.log("Server is running"));
