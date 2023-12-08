e4538801d9e9d515be1c7d5cc705190bc78f0256
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-07 22:23:40 -0400

    fix: remove dotenv to test debloy vercel

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import { app } from "./app";
 import { connect } from "./database/connect";
-require("dotenv").config();
 
 connect().then(() => {
   app.listen(process.env.PORT, () => console.log("Server is running"));
