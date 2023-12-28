7139a8011d967f6d64268e0f5b49d62305d9b37c
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-27 21:17:41 -0400

    fix: add valid request to update movement

--- src/modules/Movement/updateMovement/UpdateMovementFactory.ts
+++ src/modules/Movement/updateMovement/UpdateMovementFactory.ts
@@ -1,10 +1,18 @@
 import { MovementRepository } from "../../../repositories/repositories/MovementRepository";
+import { ItemRepository } from "../../../repositories/repositories/ItemRepository";
+import { TypeMovementRepository } from "../../../repositories/repositories/TypeMovementRepository";
 import { UpdateMovementController } from "./UpdateMovementController";
 import { UpdateMovementService } from "./UpdateMovementService";
 
 export const updateMovementFactory = () => {
   const movementRepository = new MovementRepository();
-  const updateMovement = new UpdateMovementService(movementRepository);
+  const typeMovementRepository = new TypeMovementRepository();
+  const itemRepository = new ItemRepository();
+  const updateMovement = new UpdateMovementService(
+    movementRepository,
+    typeMovementRepository,
+    itemRepository
+  );
   const updateMovementController = new UpdateMovementController(updateMovement);
   return updateMovementController;
 };
--- src/modules/Movement/updateMovement/UpdateMovementService.ts
+++ src/modules/Movement/updateMovement/UpdateMovementService.ts
@@ -1,8 +1,14 @@
 import { Movement } from "../../../entities/Movement";
 import { IMovementRepository } from "../../../repositories/Interfaces/IMovementRepository";
+import { IItemRepository } from "../../../repositories/Interfaces/IItemRepository";
+import { ITypeMovementRepository } from "../../../repositories/Interfaces/ITypeMovementRepository";
 
 export class UpdateMovementService {
-  constructor(private movementRepository: IMovementRepository) {}
+  constructor(
+    private movementRepository: IMovementRepository,
+    private typeMovementRepository: ITypeMovementRepository,
+    private itemRepository: IItemRepository
+  ) {}
 
   async execute({
     id,
@@ -11,6 +17,28 @@ export class UpdateMovementService {
     type_movement_id,
     item_id,
   }): Promise<Movement | Error> {
+    try {
+      await this.validRequest({
+        description,
+        quantity,
+        type_movement_id,
+        item_id,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return new Error(error.message);
+      }
+    }
+    const item = await this.itemRepository.getOne(item_id);
+    if (item instanceof Error) {
+      return new Error(item.message);
+    }
+    const typeMovement = await this.typeMovementRepository.getOne(
+      type_movement_id
+    );
+    if (typeMovement instanceof Error) {
+      return new Error(typeMovement.message);
+    }
     const result = await this.movementRepository.update({
       id,
       description,
@@ -24,5 +52,29 @@ export class UpdateMovementService {
     }
 
     return result;
+  }
+  async validRequest({
+    description,
+    quantity,
+    type_movement_id,
+    item_id,
+  }): Promise<Error | void> {
+    return new Promise((resolve, reject) => {
+      const checkArgument = (arg, argName) => {
+        if (!arg) {
+          reject(new Error(`Request missing arguments: ${argName}`));
+        }
+      };
+
+      try {
+        checkArgument(description, "description");
+        checkArgument(quantity, "quantity");
+        checkArgument(type_movement_id, "type_movement_id");
+        checkArgument(item_id, "item_id");
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 }
