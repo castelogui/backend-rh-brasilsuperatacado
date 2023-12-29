71a55095dda1b84ad19e6081b8406dc08c9c8aff
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-29 10:06:41 -0400

    tests: add new test to GET Categories

--- __tests__/integration/category.test.ts
+++ __tests__/integration/category.test.ts
@@ -3,10 +3,14 @@ import { Category } from "../../src/entities/Category";
 import { app } from "../../src/app";
 import { AppDataSource } from "../../src/database/AppDataSource";
 
+function parseResponse(response: any, type: string) {
+  return JSON.parse(response.text.substring(response.text.indexOf(type)));
+}
+
 beforeAll(async () => {
   await AppDataSource.initialize();
 });
-describe("Category", () => {
+describe("Category => create", () => {
   it("should be create a new category", async () => {
     const category = AppDataSource.getRepository(Category).create({
       name: "Camisa",
@@ -27,7 +31,6 @@ describe("Category", () => {
       description: "camisa",
     });
     const response = await supertest(app).post("/categories").send(category);
-    console.log(response.text);
 
     expect(response.status).toBe(400);
     expect(response.text).toBe('"Category already exists"');
@@ -38,13 +41,44 @@ describe("Category", () => {
       name: "Calça",
     });
     const response = await supertest(app).post("/categories").send(category);
-    console.log(response.text);
 
     expect(response.status).toBe(200);
     expect(response.text).toContain("id");
     expect(response.text).toContain("name");
     expect(response.text).toContain("description");
     expect(response.text).toContain("created_at");
+  });
+});
+describe("Category => get", () => {
+  it("a list category should be searched", async () => {
+    const category = AppDataSource.getRepository(Category).create({
+      name: "Botina",
+    });
+    const created = await supertest(app).post("/categories").send(category);
+    const id = parseResponse(created, "{").id;
+
+    const response = await supertest(app).get(`/categories/${id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("description");
+    expect(response.body).toHaveProperty("created_at");
+  });
+  it("a list of categories must be returned", async () => {
+    const response = await supertest(app).get("/categories");
+    expect(response.status).toBe(200);
+
+    expect(Array.isArray(response.body)).toBe(true);
+
+    if (Array.isArray(response.body)) {
+      response.body.forEach((category: any) => {
+        expect(category).toHaveProperty("id");
+        expect(category).toHaveProperty("name");
+        expect(category).toHaveProperty("description");
+        expect(category).toHaveProperty("created_at");
+      });
+    }
   });
 });
 afterAll(async () => {
