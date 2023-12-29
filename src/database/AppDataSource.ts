34f89b26852937672ece5bd5af5282636bb0caf0
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-29 00:52:49 -0400

    tests: add appdatasourcetest

--- src/database/AppDataSource.ts
+++ src/database/AppDataSource.ts
@@ -11,3 +11,14 @@ export const AppDataSource = new DataSource({
   logging: true,
   synchronize: true,
 });
+
+export const AppDataSourceTest = new DataSource({
+  type: "sqlite",
+  database: "./__tests__/database.sqlite",
+  //url: process.env.DATABASE_URL,
+  entities: ["src/entities/*.{js,ts}"],
+  migrations: ["src/database/migrations/*.{js,ts}"],
+  migrationsTableName: "migration",
+  logging: true,
+  synchronize: true,
+});
