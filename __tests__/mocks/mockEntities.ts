02000edea5972027695126bc79b5ba1971224935
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-12-31 01:54:59 -0400

    tests: add mocks to entitie Color

--- __tests__/mocks/mockEntities.ts
+++ __tests__/mocks/mockEntities.ts
@@ -1,7 +1,9 @@
 import { AppDataSource } from "../../src/database/AppDataSource";
 import { Category } from "../../src/entities/Category";
+import { Color } from "../../src/entities/Color";
 
 const categoryRepository = AppDataSource.getRepository(Category);
+const colorRepository = AppDataSource.getRepository(Color);
 
 export class CategoryMock {
   category_1() {
@@ -21,5 +23,26 @@ export class CategoryMock {
   }
   category_6() {
     return categoryRepository.create({ name: "Luvas", description: "" });
+  }
+}
+
+export class ColorMock {
+  color_1() {
+    return colorRepository.create({ name: "Azul", hexadecimal: "#00f" });
+  }
+  color_2() {
+    return colorRepository.create({ name: "Vermelho", hexadecimal: "#f00" });
+  }
+  color_3() {
+    return colorRepository.create({ name: "Branco", hexadecimal: "#fff" });
+  }
+  color_4() {
+    return colorRepository.create({ name: "Verde", hexadecimal: "#0f0" });
+  }
+  color_5() {
+    return colorRepository.create({ name: "Preto", hexadecimal: "#000" });
+  }
+  color_6() {
+    return colorRepository.create({ name: "Amarelo", hexadecimal: "#0ff" });
   }
 }
