a96d8d6165cd92bcf46a5bc43453c96ac3f852e9
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-04 17:47:08 -0400

    refactor: remove sqlite

--- src/database/AppDataSource.ts
+++ src/database/AppDataSource.ts
@@ -11,13 +11,3 @@ export const AppDataSource = new DataSource({
   logging: false,
   synchronize: true,
 });
-
-export const AppDataSourceTest = new DataSource({
-  type: "sqlite",
-  database: "./__tests__/database.sqlite",
-  //url: process.env.DATABASE_URL,
-  entities: ["src/entities/*.{js,ts}"],
-  migrations: ["src/database/migrations/*.{js,ts}"],
-  logging: true,
-  synchronize: true,
-});
