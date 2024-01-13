b561f060ce26959c54169083f6a078adee37d6e0
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-13 12:03:59 -0400

    tests: update test movement

--- __tests__/integration/Movement/movement.test.ts
+++ __tests__/integration/Movement/movement.test.ts
@@ -1,6 +1,8 @@
 import supertest from "supertest";
 import { MockAppDataSource } from "../../mocks/mockAppDataSource";
 import { app } from "../../../src/app";
+import path from "path";
+const fs = require("fs").promises;
 
 function expect200(response: any) {
   return Object.keys(response.body).forEach((key) => {
@@ -9,43 +11,69 @@ function expect200(response: any) {
     expect(response.body[key]).not.toBeNull();
   });
 }
+function expectEstoque(response: any, item: any) {
+  return expect(item.body.estoque).toBe(
+    response.body.quantity + response.body.item_estoque_ant
+  );
+}
 
 const mockAppDataSource = new MockAppDataSource();
-const mockData = JSON.parse("./mock.json");
+
+const presetData = async () => {
+  try {
+    const mockDataPath = path.join(__dirname, "mock.json");
+    const mockData = JSON.parse(await fs.readFile(mockDataPath, "utf-8"));
+
+    mockData.forEach(async (obj: Object) => {
+      Object.keys(obj).forEach((key) => {
+        obj[key].forEach(async (o) => {
+          await supertest(app)
+            .post(`/${key}`)
+            .send(o)
+            .then((response) => {
+              console.log("Created: ", key, response.body);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      });
+    });
+
+    //for (const category of mockData.categories) {
+    //  await supertest(app)
+    //    .post("/categories")
+    //    .send(category)
+    //    .catch((err) => {
+    //      console.log(err);
+    //    });
+    //}
+  } catch (error) {
+    console.error(`Erro ao carregar o arquivo JSON: ${error.message}`);
+  }
+};
 
 beforeAll(async () => {
   await mockAppDataSource.connect();
-  await supertest(app)
-    .post("/categories")
-    .send({ id: "1", name: "Camisas", description: "camisas" });
-  await supertest(app)
-    .post("/categories")
-    .send({ id: "2", name: "Calças", description: "calças" });
-  await supertest(app)
-    .post("/colors")
-    .send({ id: "1", name: "Branca", hexadecimal: "#fff" });
-  await supertest(app)
-    .post("/colors")
-    .send({ id: "2", name: "Azul", hexadecimal: "#00f" });
-  await supertest(app).post("/items").send({
-    id: "1",
-    name: "Uniforme Adm",
-    category_id: "1",
-    color_id: "1",
-    size: "M",
+  await presetData();
+});
+describe("Movement => create", () => {
+  it("should be create a movement entrada => success", async () => {
+    const response = await supertest(app).post("/movements").send({
+      description: "Entrada de uniformes",
+      quantity: 5,
+      type_movement_id: "1",
+      item_id: "1",
+    });
+
+    const item = await supertest(app).get("/items/1");
+
+    expect200(response);
+    expect200(item);
+    expectEstoque(response, item);
   });
-  await supertest(app).post("/items").send({
-    id: "2",
-    name: "Uniforme Adm",
-    category_id: "1",
-    color_id: "2",
-    size: "M",
-  });
-  await supertest(app).post("/items").send({
-    id: "3",
-    name: "Uniforme Piso",
-    category_id: "1",
-    color_id: "2",
-    size: "M",
-  });
+});
+afterAll(async () => {
+  await mockAppDataSource.drop();
+  await mockAppDataSource.destroy();
 });
