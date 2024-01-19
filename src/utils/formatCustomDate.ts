1708caaa88addc343328aa54cb9b75b466521bfa
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-19 09:26:56 -0400

    fix: adjust format hour

--- src/utils/formatCustomDate.ts
+++ src/utils/formatCustomDate.ts
@@ -1,7 +1,10 @@
 function toISOFormat(dateTimeString: string) {
   const [date, time] = dateTimeString.split(", ");
   const [DD, MM, YYYY] = date.split("/");
-  const [HH, mm, ss] = time.split(":");
+  let [HH, mm, ss] = time.split(":");
+  HH.length == 1 ? (HH = "0" + HH) : HH;
+  mm.length == 1 ? (mm = "0" + mm) : mm;
+  ss.length == 1 ? (ss = "0" + ss) : ss;
   return `${YYYY}-${MM}-${DD}T${HH}:${mm}:${ss}.040Z`;
 }
 
@@ -10,7 +13,7 @@ export class FormatCustomDate {
     const date = new Date().toLocaleString("pt-BR", {
       timeZone: "America/Manaus",
     });
-    
+
     return toISOFormat(date);
   }
 }
