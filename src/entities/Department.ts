e4ffb3b3a235b33c52a2ff803078d2674fb366be
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-19 14:32:20 -0400

    fix: update entity department

--- src/entities/Department.ts
+++ src/entities/Department.ts
@@ -2,16 +2,20 @@ import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 import { FormatCustomDate } from "../utils/formatCustomDate";
 
-@Entity("departaments")
-export class Departament {
+@Entity("departments")
+export class Department {
   @PrimaryColumn()
   id: string;
+  @Column()
+  codigo: string;
   @Column()
   name: string;
   @Column()
   description: string;
   @CreateDateColumn()
   created_at: Date;
+  @CreateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id || this.id == undefined || this.id == "") {
@@ -19,6 +23,9 @@ export class Departament {
     }
     if (!this.created_at || this.created_at == undefined) {
       this.created_at = new Date(new FormatCustomDate().dateTimeLocal())
+    }
+    if (!this.updated_at || this.updated_at == undefined) {
+      this.updated_at = new Date(new FormatCustomDate().dateTimeLocal())
     }
   }
 }
